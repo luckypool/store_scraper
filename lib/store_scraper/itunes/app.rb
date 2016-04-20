d2fc06909deca0ade092d34170cc6ff1756af701
YutaYoshiike <yuta.yoshiike@mixi.co.jp>
2016-04-20 22:45:53 +0900

    Add review/app

---
+++ lib/store_scraper/itunes/app.rb
@@ -0,0 +1,4 @@
+
+# https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+#
+# https://itunes.apple.com/lookup?id=284910350
---
+++ lib/store_scraper/itunes/review.rb
@@ -0,0 +1,3 @@
+# https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+# https://itunes.apple.com/jp/rss/customerreviews/id=<iOS App ID>/json
+
