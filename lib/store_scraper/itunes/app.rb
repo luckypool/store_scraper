51db2bdedcb754aa86da416434b4b5f7bbe16109
luckypool <luckypool314@gmail.com>
2016-04-21 02:10:33 +0900

    Add default param

--- lib/store_scraper/itunes/app.rb
+++ lib/store_scraper/itunes/app.rb
@@ -10,12 +10,12 @@ module StoreScraper::Itunes
 
     # https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
     # ex.) https://itunes.apple.com/lookup?id=284910350
-    def initialize(country)
+    def initialize(country = 'jp')
       @country = validate_country(country)
       @client  = ::StoreScraper::Client.new(URL_PREFIX)
     end
 
-    def find(id)
+    def find(id = 658511662) # モンスト
       path = build_path(id)
       response = client.get(path)
       response.body
