2d9883134592b58079946244ddc1a1bcbbcac7a5
YutaYoshiike <yuta.yoshiike@mixi.co.jp>
2016-04-21 11:46:54 +0900

    Fix ABC

--- lib/store_scraper/itunes/ranking/entry.rb
+++ lib/store_scraper/itunes/ranking/entry.rb
@@ -2,18 +2,44 @@ module StoreScraper
   module Itunes
     class Ranking
       class Entry
-        attr_reader :id, :title, :bundle_id, :category, :image, :publisher, :link, :summary
         attr_reader :rank
+        attr_reader :raw_entry
+
         def initialize(raw_entry, rank)
-          @id        = raw_entry.id.attributes['im:id'].to_i
-          @title     = raw_entry['im:name'].label
-          @bundle_id = raw_entry.id.attributes['im:bundleId']
-          @category  = raw_entry.category.attributes.label
-          @image     = raw_entry['im:image'].first.label
-          @publisher = raw_entry['im:artist'].label
-          @link      = raw_entry.link.attributes.href
-          @summary   = raw_entry.summary.label
+          @raw_entry = raw_entry
           @rank      = rank
+        end
+
+        def id
+          raw_entry.id.attributes['im:id'].to_i
+        end
+
+        def title
+          raw_entry['im:name'].label
+        end
+
+        def bundle_id
+          raw_entry.id.attributes['im:bundleId']
+        end
+
+        def category
+          raw_entry.category.attributes.label
+        end
+
+        def image
+          raw_entry['im:image'].first.label
+        end
+
+        def publisher
+          raw_entry['im:artist'].label
+        end
+
+        def link
+          raw_entry.link.attributes.href
+        end
+
+        def summary
+          raw_entry.summary.label
         end
       end
     end
