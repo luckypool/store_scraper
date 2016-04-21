6fc227b9266cf0fb947d2fcdb500b4685996d900
YutaYoshiike <yuta.yoshiike@mixi.co.jp>
2016-04-21 11:24:54 +0900

    Remove shorted module namespace

--- lib/store_scraper/itunes/app.rb
+++ lib/store_scraper/itunes/app.rb
@@ -1,25 +1,27 @@
-module StoreScraper::Itunes
-  class App
-    include Constants
-    include Validator
+module StoreScraper
+  module Itunes
+    class App
+      include Constants
+      include Validator
 
-    attr_reader :country, :client
+      attr_reader :country, :client
 
-    # https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
-    # ex.) https://itunes.apple.com/lookup?id=284910350
-    def initialize(country = 'jp')
-      @country = validate_country(country)
-      @client  = ::StoreScraper::Client.new(URL_PREFIX)
-    end
+      # https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+      # ex.) https://itunes.apple.com/lookup?id=284910350
+      def initialize(country = 'jp')
+        @country = validate_country(country)
+        @client  = ::StoreScraper::Client.new(URL_PREFIX)
+      end
 
-    def find(id = 658511662) # モンスト
-      path = build_path(id)
-      response = client.get(path)
-      response.body
-    end
+      def find(id = 658511662) # モンスト
+        path = build_path(id)
+        response = client.get(path)
+        response.body
+      end
 
-    def build_path(id)
-      "/#{country}/lookup?id=#{id}"
+      def build_path(id)
+        "/#{country}/lookup?id=#{id}"
+      end
     end
   end
 end
